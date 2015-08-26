c113217e0cafaed235b89df6c449a4079e8ea51a
Deano Calver <deano@cloudpixies.com>
2015-08-25 21:32:21 -0700

    Added a PLL timing calculator for when icestorm supports the PLLs

--- ice40ktool.cpp
+++ ice40ktool.cpp
@@ -7,18 +7,23 @@
 #include <iterator>
 #include <iomanip>
 #include <cmath>
+#include <limits>
+#include <cstdlib>
+#include <cassert>
+
 static const char USAGE[] =
 R"(iCE40K Tools.
 	Generic tool for pre-processing stuff for the iCE40K system
 
 	Usage:
+	  ice40ktool pllcalc <refclk> <desiredclk>
 	  ice40ktool mkrom <infile> <outfile> 
 	  ice40ktool (-h | --help)
 	  ice40ktool --version
 
 	Options:
-	  -h --help     Show this screen.
-	  --version     Show version.
+	  -h --help     	Show this screen.
+	  --version     	Show version.
 )";
 
 static const char VERSION[] = "ice40ktool 0.0.0";
@@ -31,9 +36,11 @@ int main( int argc, char** argv )
 						 true,               // show help if requested
 						 VERSION );  // version string
 
-	for(auto const& arg : args) {
-		std::cout << arg.first << " = " << arg.second << std::endl;
-	}
+// comment in for command line debugging 
+//	for(auto const& arg : args) {
+//		std::cout << arg.first << " = " << arg.second << std::endl;
+//	}
+
 	if( args["mkrom"].isBool() && args["mkrom"].asBool() == true ) {
 		if( args["<infile>"].isString() == true && args["<outfile>"].isString() == true)
 		{
@@ -93,6 +100,81 @@ int main( int argc, char** argv )
 		{
 			std::cout << "Error: Usage should be ice40ktool mkrom <in file> <out file>\n";
 		}
+	} else
+	if( args["pllcalc"].isBool() && args["pllcalc"].asBool() == true )
+	{
+		if( args["<refclk>"].isString() && args["<desiredclk>"].isString() )
+		{
+			const double desiredclk = static_cast<double>( atol( args["<desiredclk>"].asString().c_str() ) );
+			const double refclk = static_cast<double>( atol( args["<refclk>"].asString().c_str() ) );
+			long bestr = -1, bestf = -1;
+			double bestdist = std::numeric_limits<double>::infinity();
+			double bestfreq = std::numeric_limits<double>::infinity();
+
+			// exhaustively search non simple mode first
+			for( long divr = 0; divr <= 15; ++divr)
+			{
+				for( long divf = 0; divf <= 63; ++divf )
+				{
+					// F_PLLOUT = (F_REFCLK * (DIVF+1)) / (DIVR+1)
+					double pllout = (refclk * static_cast<double>(divf+1)) / static_cast<double>(divr+1);
+					if( std::abs(pllout - desiredclk) < 1e-6 ) 
+					{
+						// spot on, we will take it
+						std::cout << "Perfect frequency match for " << atol( args["<desiredclk>"].asString().c_str() ) << "hz : divf = " << divf << " divr = " << divr << " divq = 0\n";
+						exit(0);
+					} else if( std::abs(pllout - desiredclk) < bestdist )
+					{
+						// update and continue search
+						bestdist = std::abs(pllout - desiredclk);
+						bestfreq = pllout;
+						bestr = divr;
+						bestf = divf;
+					}
+				}
+			}
+			// output non simple mode search 
+			assert( bestr != -1 && bestf != -1 );
+			std::cout << "Best non simple mode PLL is a frequency of " << bestfreq << "hz : divf = " << bestf << " divr = " << bestr << "\n";
+
+			bestr = -1, bestf = -1;
+			long bestq = -1;
+			bestdist = std::numeric_limits<double>::infinity();
+			bestfreq = std::numeric_limits<double>::infinity();
+			// exhaustively search simple mode first
+			for( long divr = 0; divr <= 15; ++divr)
+			{
+				for( long divf = 0; divf <= 63; ++divf )
+				{
+					for( long divq = 0; divq <= 7; ++divq ) {
+						// F_PLLOUT = (F_REFCLK * (DIVF+1)) / (2^DIVQ)*(DIVR+1)
+						double pllout = (refclk * static_cast<double>(divf+1)) / (static_cast<double>(1 << divq) * static_cast<double>(divr+1) );
+						if( std::abs(pllout - desiredclk) < 1e-6 ) 
+						{
+							// spot on, we will take it
+							std::cout << "Perfect simple mode frequency match for " << atol( args["<desiredclk>"].asString().c_str() ) << "hz :  divf = " << divf << " divr = " << divr << " divq = " << divq << "\n";
+							exit(0);
+						} else if( std::abs(pllout - desiredclk) < bestdist )
+						{
+							// update and continue search
+							bestdist = std::abs(pllout - desiredclk);
+							bestfreq = pllout;
+							bestr = divr;
+							bestf = divf;
+							bestq = divq;
+						}
+					}
+				}
+			}
+			// output simple mode search 
+			assert( bestr != -1 && bestf != -1 && bestq != -1 );
+			std::cout << "Best simple mode PLL is a frequency of " << bestfreq << "hz : divf = " << bestf << " divr = " << bestr << " divq = " << bestq << "\n";
+
+		} else 
+		{
+			std::cout << "Error: Usage should be ice40ktool pllcalc reference clock (number) desired clock (number)\n";			
+		}
+
 	} else
 	{
 		std::cout << "Unknown tool command\n";
